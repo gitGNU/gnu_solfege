f983cef4f93b50a5e56d639805328c5c0847e495
Tom Cato Amundsen <tca@gnu.org>
2013-04-01 23:41:48 +0200

    tweak win32 binary creation

--- make-win32-bindir.sh
+++ make-win32-bindir.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIST=winbuild
+DIST=build
 PYGI=/C/mygi
 PYVER=27
 
@@ -15,17 +15,33 @@ trim() {
 	rm -rf tcl
 	rm -rf Tools
 	rm -rf include
+	rm -rf Lib/bsddb
 	rm -rf Lib/compiler
+	rm -rf Lib/distutils
+	rm -rf Lib/email
+	rm -rf Lib/importlib
 	rm -rf Lib/lib2to3
 	rm -rf Lib/lib-tk
 	rm -rf Lib/hotshot
 	rm -rf Lib/idlelib
 	rm -rf Lib/json
 	rm -rf Lib/msilib
+	rm -rf Lib/pydoc_data
 	rm -rf Lib/test
 	rm -rf Lib/wsgiref
 	rm -rf Lib/curses
 	rm -rf Lib/unittest
+	mkdir Python.txt
+	mv LICENSE.txt README.txt NEWS.txt Python.txt
+	rm python-2.7.2.msi
+	rm -rf Lib/site-packages/gtk/share/dbus-1
+	rm -rf Lib/site-packages/gtk/share/devhelp
+	rm -rf Lib/site-packages/gtk/share/enchant
+	rm -rf Lib/site-packages/gtk/share/gedit
+	rm -rf Lib/site-packages/gtk/share/glade
+	rm -rf Lib/site-packages/gtk/share/gtksourceview-3.0
+	rm -rf Lib/site-packages/gtk/share/gtranslator
+	rm -rf Lib/site-packages/gtk/share/telepathy
 	cd ..
 }
 copy_mygi() {
@@ -35,8 +51,10 @@ copy_mygi() {
 setup
 copy_mygi
 trim
+make winbuild
+mv AUTHORS.txt COPYING.txt FAQ.txt INSTALL.txt INSTALL.win32.txt README.txt $DIST
 make DESTDIR=$DIST prefix="" install
 cp solfege/soundcard/winmidi.pyd $DIST/share/solfege/solfege/soundcard
-cp win32-start-solfege.pyw winbuild/bin/
-cp debugsolfege.bat winbuild/bin
+cp win32-start-solfege.pyw $DIST/bin/
+cp debugsolfege.bat $DIST/bin
 echo "done!"
