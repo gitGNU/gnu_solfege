825f4126be2e23f2b1eb8a02e74a7d310184d0ed
Tom Cato Amundsen <tca@gnu.org>
2013-09-28 00:58:03 +0200

    clean up autogen.sh
    autogen.sh should not run configure, and should not setup
    environment variables needed to run configure on win32

--- autogen.sh
+++ autogen.sh
@@ -1,29 +1,6 @@
 #!/bin/bash
 set -e
 
-# To run this script on MS Windows you might have to check that the
-# paths for PKG_CONFIG_PATH and ACLOCAL_INCLUDE is correctly.
-if [ "$OSTYPE" = "msys" ]; then
-    export PKG_CONFIG_PATH=/c/GTK/lib/pkgconfig:/c/python25/lib/pkgconfig
-    export ACLOCAL_INCLUDE=" -I /c/GTK/share/aclocal"
-fi
-
-
 aclocal $ACINCLUDE
-
 autoconf
-
-if [ "$OSTYPE" = "msys" ]; then	
-  PYTHON=/c/python27/python.exe GIT=C:/Programfiler/Git/bin/git.exe ./configure --enable-winmidi PYTHONDLL=/C/WINDOWS/system32/python27.dll $CONFIGURE_OPTS $@
-else
-  ./configure $CONFIGURE_OPTS $@
-fi
-
-make solfege/_version.py
-make solfege/languages.py
-
-echo
-echo "You can now run solfege with no further action from the source directory."
-echo "The user manual and some lesson files will be missing until you build it"
-echo "by running 'make'. The missing lesson files will generate some warnings"
-echo "that you should ignore. Don't report them as bugs!"
+python -c "import tools.buildutil; tools.buildutil.create_versions_file('git')"
