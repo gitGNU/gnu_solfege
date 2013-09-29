2cc30bb94feab721c36409d5b7e25e2c33f91951
Tom Cato Amundsen <tca@gnu.org>
2013-09-30 00:42:54 +0200

    make runsrcdir.bat usable
    fix to work with the new way we build the installer on windows.

--- runsrcdir.bat
+++ runsrcdir.bat
@@ -4,11 +4,14 @@ rem Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2013  Tom Cato Amundsen
 rem The contents of langenviron.bat is set by configwindow.py
 rem and is used to select the language to use.
 
-rem For this script to work, you must setup thinks as described
+rem For this script to work, you must setup things as described
 rem in INSTALL.win32
 
 IF EXIST "%APPDATA%\GNU Solfege\langenviron.bat" (call "%APPDATA%\GNU Solfege\langenviron.bat")
 
-set PATH=C:\Python27\Lib\site-packages\gtk
-C:\python27\python solfege.py
+rem set PATH=..\mypython\Lib\site-packages\gtk
+set PATH=win32\python\Lib\site-packages\gtk
+
+win32\python\python.exe solfege.py
+rem ..\mypython\python.exe solfege.py
 pause
