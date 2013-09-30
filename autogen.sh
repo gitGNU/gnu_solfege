fe38285bacc791cc2a0101119f8c3852e5386c8f
Tom Cato Amundsen <tca@gnu.org>
2013-09-30 12:48:47 +0200

    win32 build fixes
    Tweaking autogen.sh, configure.ac, Makefile.in and winbuild.sh
    Not done yet, but it works.

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
-aclocal $ACINCLUDE
-autoconf
-python -c "import tools.buildutil; tools.buildutil.create_versions_file('git')"
+if [ "$OSTYPE" = "msys" ]; then
+	export GIT=C:/Programfiler/Git/bin/git.exe
+	export PYTHON=/c/python27/python.exe
+	export PYTHON=../mypython/python.exe
+	aclocal-1.11
+	autoconf
+	configure --enable-winmidi
+	$PYTHON -c "import tools.buildutil; tools.buildutil.create_versions_file('$GIT')"
+else
+	aclocal $ACINCLUDE
+	autoconf
+        python -c "import tools.buildutil; tools.buildutil.create_versions_file('git')"
+fi
--- winbuild.sh
+++ winbuild.sh
@@ -9,43 +9,47 @@ setup() {
   # running from the source dir, and for the installer.
   rm win32 -rf
   mkdir win32
-  mkdir win32/python
-  (cd win32/python && tar zxf ../../../my-python-$MYPYVER.tgz)
-  mv win32/python/DLLs win32/bin
-  mv win32/python/Lib win32/bin
-  mv win32/python/libs win32/bin
-  mv win32/python/LICENSE.txt win32/LICENSE.python.txt
-  mv win32/python/NEWS.txt win32/NEWS.python.txt
-  mv win32/python/README.txt win32/README.python.txt
-  mv win32/python/python.exe win32/bin
-  mv win32/python/pythonw.exe win32/bin
-  mv win32/python/w9xpopen.exe win32/bin
-  mv win32/python/python25.dll win32/bin
-  mv win32/python/msvcr71.dll win32/bin
-  $PYTHON tools/get-gtk-files.py get-bundle
-  $PYTHON tools/get-gtk-files.py unpack-bundle
-  $PYTHON tools/get-gtk-files.py bin
-  $PYTHON tools/get-gtk-files.py unpack
-  mv win32/bin/libxml2.dll win32/bin/libxml2-2.dll
-  mv win32/bin/libiconv2.dll win32/bin/iconv.dll
+  cp -a ../mypython win32/python
+  #mkdir win32/python
+  #(cd win32/python && tar zxf ../../../my-python-$MYPYVER.tgz)
+  #mv win32/python/DLLs win32/bin
+  #mv win32/python/Lib win32/bin
+  #mv win32/python/libs win32/bin
+  #mv win32/python/LICENSE.txt win32/LICENSE.python.txt
+  #mv win32/python/NEWS.txt win32/NEWS.python.txt
+  #mv win32/python/README.txt win32/README.python.txt
+  #mv win32/python/python.exe win32/bin
+  #mv win32/python/pythonw.exe win32/bin
+  #mv win32/python/w9xpopen.exe win32/bin
+  #mv win32/python/python25.dll win32/bin
+  #mv win32/python/msvcr71.dll win32/bin
+  #$PYTHON tools/get-gtk-files.py get-bundle
+  #$PYTHON tools/get-gtk-files.py unpack-bundle
+  #$PYTHON tools/get-gtk-files.py bin
+  #$PYTHON tools/get-gtk-files.py unpack
+  #mv win32/bin/libxml2.dll win32/bin/libxml2-2.dll
+  #mv win32/bin/libiconv2.dll win32/bin/iconv.dll
 
-  echo '"lib/gtk-2.0/2.10.0/loaders/svg_loader.dll"' > win32/etc/gtk-2.0/gdk-pixbuf.loaders
-  echo '"svg" 2 "gdk-pixbuf" "Scalable Vector Graphics" "LGPL"' >> win32/etc/gtk-2.0/gdk-pixbuf.loaders 
-  echo '"image/svg+xml" "image/svg" "image/svg-xml" "image/vnd.adobe.svg+xml" "text/xml-svg" "image/svg+xml-compressed" ""' >> win32/etc/gtk-2.0/gdk-pixbuf.loaders 
-  echo '"svg" "svgz" "svg.gz" ""' >> win32/etc/gtk-2.0/gdk-pixbuf.loaders 
-  echo '" <svg" "*    " 100' >> win32/etc/gtk-2.0/gdk-pixbuf.loaders 
-  echo '" <!DOCTYPE svg" "*             " 100' >> win32/etc/gtk-2.0/gdk-pixbuf.loaders 
-  echo ' ' >> win32/etc/gtk-2.0/gdk-pixbuf.loaders
-  # We did the above instead of using gdk-pixbuf-query-loaderse.exe because
-  # we need to make a relocatable file.
-  #win32/bin/gdk-pixbuf-query-loaders.exe win32/lib/gtk-2.0/2.10.0/loaders/svg_loader.dll > win32/etc/gtk-2.0/gdk-pixbuf.loaders 
+  #echo '"lib/gtk-2.0/2.10.0/loaders/svg_loader.dll"' > win32/etc/gtk-2.0/gdk-pixbuf.loaders
+  #echo '"svg" 2 "gdk-pixbuf" "Scalable Vector Graphics" "LGPL"' >> win32/etc/gtk-2.0/gdk-pixbuf.loaders 
+  #echo '"image/svg+xml" "image/svg" "image/svg-xml" "image/vnd.adobe.svg+xml" "text/xml-svg" "image/svg+xml-compressed" ""' >> win32/etc/gtk-2.0/gdk-pixbuf.loaders 
+  #echo '"svg" "svgz" "svg.gz" ""' >> win32/etc/gtk-2.0/gdk-pixbuf.loaders 
+  #echo '" <svg" "*    " 100' >> win32/etc/gtk-2.0/gdk-pixbuf.loaders 
+  #echo '" <!DOCTYPE svg" "*             " 100' >> win32/etc/gtk-2.0/gdk-pixbuf.loaders 
+  #echo ' ' >> win32/etc/gtk-2.0/gdk-pixbuf.loaders
+  ## We did the above instead of using gdk-pixbuf-query-loaderse.exe because
+  ## we need to make a relocatable file.
+  ##win32/bin/gdk-pixbuf-query-loaders.exe win32/lib/gtk-2.0/2.10.0/loaders/svg_loader.dll > win32/etc/gtk-2.0/gdk-pixbuf.loaders 
 
-  #mv win32/zlib-1.2.4/zlib1.dll win32/bin
-  # Move these so CSound can find python25.dll
-  cp -a ../pygtk-stuff/* win32/bin/lib/site-packages
+  ##mv win32/zlib-1.2.4/zlib1.dll win32/bin
+  ## Move these so CSound can find python25.dll
+  #cp -a ../pygtk-stuff/* win32/bin/lib/site-packages
 
-  cp testgtkenv.bat testgtkenv.py win32/bin/
-  echo "gtk-theme-name = \"MS-Windows\""  > win32/etc/gtk-2.0/gtkrc
+  #cp testgtkenv.bat testgtkenv.py win32/bin/
+  #echo "gtk-theme-name = \"MS-Windows\""  > win32/etc/gtk-2.0/gtkrc
+  find win32 -name "*.def" | xargs rm
+  find win32 -name "*.a" | xargs rm
+  find win32 -name "*.lib" | xargs rm
   (cd win32 && find -name *.pyc | xargs rm)
 }
 
@@ -56,24 +60,18 @@ build() {
   mv default.config tmp.cfg
   sed -e "s/type=external-midiplayer/type=sequencer-device/" -e "s/csound=csound/csound=AUTODETECT/" -e "s/mma=mma/mma=AUTODETECT/" tmp.cfg > default.config
   rm tmp.cfg
-  ./configure PYTHON=win32/bin/python.exe --disable-pygtk-test --enable-winmidi
-  # we build the "all" target, and not the default one that rebuilds
-  # some generated user manual .ly files because gettext.GNUTranslations
-  # on my windows machine cannot read the .mo files.
-  PATH=win32/bin:$PATH make PYTHONPATH=win32/bin/lib/site-packages/gtk-2.0 skipmanual=yes PYTHON_INCLUDES=-Iwin32/python/include all
+  ./configure PYTHON=win32/python/python.exe --enable-winmidi
+  make
   make winbuild
   cp README.txt INSTALL.win32.txt INSTALL.txt AUTHORS.txt COPYING.txt win32
 }
 install() {
   # Step 3: Install solfege into win32/ so that we can run from inside
   # it, or create the installer.
-  find win32 -name "*.def" | xargs rm
-  find win32 -name "*.a" | xargs rm
-  find win32 -name "*.lib" | xargs rm
-  $PYTHON tools/trim_win32_installer.py
-  rm -rf win32/python
-  PATH=win32/bin:$PATH make PYTHONPATH=win32/bin/lib/site-packages/gtk-2.0 DESTDIR=win32 prefix="" install skipmanual=yes PYTHON_INCLUDES=-Iwin32/python/includee
-  (cp solfege/soundcard/winmidi.pyd win32/share/solfege/solfege/soundcard)
+  #$PYTHON tools/trim_win32_installer.py
+  make DESTDIR=win32 prefix="" install skipmanual=yes
+
+  cp solfege/soundcard/winmidi.pyd win32/share/solfege/solfege/soundcard
   cp win32-start-solfege.pyw win32/bin
   cp solfegedebug.bat win32/bin/
 }
