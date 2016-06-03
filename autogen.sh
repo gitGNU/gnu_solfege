21eab7a17527a16c30050949b7ce68b1f380c59b
Tom Cato Amundsen <tca@gnu.org>
2016-06-03 15:03:49 +0200

    win32 build fixes

--- autogen.sh
+++ autogen.sh
@@ -3,12 +3,11 @@ set -e
 
 if [ "$OSTYPE" = "msys" ]; then
 	export GIT=C:/Programfiler/Git/bin/git.exe
-	export GIT="C:/Program Files (x86)/Git/bin/git.exe"
-	export PYTHON=/c/python27/python.exe
-	export PYTHON=../mypython/python.exe
+	export GIT="C:/Program Files/Git/bin/git.exe"
+	export PYTHON=/c/python34/python.exe
 	aclocal-1.11
 	autoconf
-	configure --enable-winmidi
+	configure --disable-winmidi
 	$PYTHON -c "import tools.buildutil; tools.buildutil.create_versions_file('$GIT')"
 else
 	aclocal $ACINCLUDE
